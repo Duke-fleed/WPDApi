b1e87653dc5812d2902679639f6b77c1f4ea36b3
Lasha Amashukeli <laamas@ttu.ee>
2017-03-07 16:48:14 +0200

    removed unneccessary files

--- PortableDevicesLib/PortableDeviceFile.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PortableDevices
-{
-    public class PortableDeviceFile : PortableDeviceObject
-    {
-        public PortableDeviceFile(string id, string name) : base(id, name)
-        {        
-        }
-    }
-}
